ac833c9c5860517761b37a52c78d42f2d7595820
shengtenghou4-star <shengtenghou4@gmail.com>
2026-07-22 13:40:34 +0800

    Expose real-data calibration API

--- src/houearth/__init__.py
+++ src/houearth/__init__.py
@@ -2,16 +2,28 @@
 
 from .core import LightCurve, PeriodicCandidate, SingleTransitEvent
 from .evaluation import CompletenessCell, SingleTransitTrial
+from .real_evaluation import (
+    NullScreenResult,
+    RealCompletenessCell,
+    RealInjectionTrial,
+    run_real_lightcurve_campaign,
+    wilson_interval,
+)
 from .search import search_periodic_transits, search_single_transits
 
 __all__ = [
     "CompletenessCell",
     "LightCurve",
+    "NullScreenResult",
     "PeriodicCandidate",
+    "RealCompletenessCell",
+    "RealInjectionTrial",
     "SingleTransitEvent",
     "SingleTransitTrial",
+    "run_real_lightcurve_campaign",
     "search_periodic_transits",
     "search_single_transits",
+    "wilson_interval",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
