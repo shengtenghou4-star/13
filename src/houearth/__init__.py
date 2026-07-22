907b79801ccd143d5ff0e376c6d7c4dc7633693e
shengtenghou4-star <shengtenghou4@gmail.com>
2026-07-22 13:12:07 +0800

    Export calibration result types

--- src/houearth/__init__.py
+++ src/houearth/__init__.py
@@ -1,14 +1,17 @@
-"""HOU-EARTH: reproducible transit discovery tooling for TESS light curves."""
+"""HOU-EARTH: an auditable transit-search and calibration toolkit."""
 
 from .core import LightCurve, PeriodicCandidate, SingleTransitEvent
+from .evaluation import CompletenessCell, SingleTransitTrial
 from .search import search_periodic_transits, search_single_transits
 
 __all__ = [
+    "CompletenessCell",
     "LightCurve",
     "PeriodicCandidate",
     "SingleTransitEvent",
+    "SingleTransitTrial",
     "search_periodic_transits",
     "search_single_transits",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
